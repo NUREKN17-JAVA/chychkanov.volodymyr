8f87330ddac63faf1d91ff53ac4738aa1b8c8bd6
Volodymyr Chychkanov <volodymyr.chychkanov@nure.ua>
2019-12-23 01:22:15 +0200

    Handler implementation for add button

--- src/main/java/ua/nure/kn/chychkanov/usermanagement/web/BrowseServlet.java
+++ src/main/java/ua/nure/kn/chychkanov/usermanagement/web/BrowseServlet.java
@@ -18,9 +18,40 @@ public class BrowseServlet extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		if (req.getParameter("add") != null) {
+	           add(req, resp);
+	       } else if (req.getParameter("edit") != null) {
+	           edit(req, resp);
+	       } else if (req.getParameter("delete") != null) {
+	           delete(req, resp);
+	       } else if (req.getParameter("details") != null) {
+	           details(req, resp);
+	       } else {
+	           browse(req, resp);
+	       }
 		browse(req, resp);
 	}
 
+	private void details(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void edit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+	
 	private void browse(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Collection users;
 		try {
